b4dc5479e65f8bc527797e1dbcb5b0edf7783c57
Manuel Sierra <86983576+ManuelSierraDeveloper@users.noreply.github.com>
2026-04-12 13:53:45 -0500

    Capa de controlador, archivos parte 1

---
+++ src/main/java/co/unimagdalena/tiendauni/controller/CategoryController.java
@@ -0,0 +1,15 @@
+package co.unimagdalena.tiendauni.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/categories")
+@RequiredArgsConstructor
+@Validated
+public class CategoryController {
+
+    //private CategoryService categoryService;
+}
